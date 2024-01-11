3b12b81665a9aa2a185d78d76f700c6a08da3d1b
akhil kodali <kodaliakhil1999@gmail.com>
2024-01-11 14:52:00 +0530

    created test routes

--- routes.js
+++ routes.js
@@ -7,6 +7,12 @@ const authMiddleware = require("./middlewares/auth-middleware");
 router.get("/", (req, res) => {
   res.send("Hello from backend");
 });
+router.get("/test1", (req, res) => {
+  res.send("Hello from test-1");
+});
+router.get("/test2", (req, res) => {
+  res.send("Hello from test-2");
+});
 router.post("/api/send-otp", authController.sendOtp);
 router.post("/api/verify-otp", authController.verifyOtp);
 router.post("/api/activate", authMiddleware, activateController.activate);
